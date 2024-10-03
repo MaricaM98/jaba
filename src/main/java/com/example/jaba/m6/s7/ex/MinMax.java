bc48a2761bedb1160e99f4017e9d899cfb8f614f
Marica Moramarco <xy@gmail.com>
2024-10-03 12:22:01 +0200

    Exercise done

---
+++ src/main/java/com/example/jaba/m6/s7/ex/MinMax.java
@@ -0,0 +1,17 @@
+package com.example.jaba.m6.s7.ex;
+
+public class MinMax {
+	public static void main(String[] args) {
+		int x = 42;
+		int y = 53;
+		int max = 0;
+		int min = 0;
+		System.out.println("The given numbers are: " + x + ", " + y);
+		max = Math.max(x, y);
+		System.out.println("Max is " + max);
+		min = Math.min(x, y);
+		System.out.println("Min is " + min);
+
+	}
+
+}
